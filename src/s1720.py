0853f11bd7282bc6d7b83f4487662332126c31af
Arturo Palau <arturo.palau@sonarsource.com>
2023-07-11 20:49:14 -0500

    s1720 python script

---
+++ src/s1720.py
@@ -0,0 +1,19 @@
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the result.
+    """
+    return a + b
+
+def main():
+    """
+    Entry point of the script.
+    """
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    result = add_numbers(num1, num2)
+    print("The sum is: {}".format(result))
+
+if __name__ == "__main__":
+    main()
+
