3b94f77a9cbf7cc3627a1ef746d02f5f8dee17ea
Arturo Palau <arturo.palau@sonarsource.com>
2023-07-11 21:02:35 -0500

    s2589 python script

---
+++ src/s2589.py
@@ -0,0 +1,15 @@
+def is_positive(number):
+    return number > 0
+
+def main():
+    value = int(input("Enter a number: "))
+
+    # Gratuitous Boolean expression
+    if is_positive(value) == True:
+        print("The number is positive.")
+    else:
+        print("The number is not positive.")
+
+if __name__ == "__main__":
+    main()
+
