e8eac6ec3495b783a87e472ae05c7cae5600a78a
Arturo Palau <arturo.palau@sonarsource.com>
2023-07-11 20:58:16 -0500

    s5756 python script

---
+++ src/s5756.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, x, y):
+        return x + y
+
+calculator = Calculator()
+result = calculator.add(5, 10)
+print("Result:", result)
+
+# Bug: Calling a non-callable value
+calculator = 42
+result = calculator.add(5, 10)
+print("Result:", result)
+
