0cc7c8150ea950143647ba222ffde04f577429b8
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2024-03-06 11:16:02 -0600

    Update S2734.py

--- src/S2734.py
+++ src/S2734.py
@@ -2,3 +2,8 @@ class MyClass(object):
     def __init__(self):
         self.message = 'Hello'
         return self  # Noncompliant: a TypeError will be raised
+
+
+
+if not a == 2:        # Noncompliant
+    b = not i < 10    # Noncompliant
