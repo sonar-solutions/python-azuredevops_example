b1071f6f003cb3f6277cbbcfa2de03a07bd14f33
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2024-03-06 15:40:01 -0600

    Update s5756.py

--- src/s5756.py
+++ src/s5756.py
@@ -19,13 +19,7 @@ print("Result:", result)
 
 name = name
 
+import tempfile
 
-
-
-parser = etree.XMLParser() # Noncompliant: by default resolve_entities is set to true
-tree1 = etree.parse('ressources/xxe.xml', parser)
-root1 = tree1.getroot()
-
-parser = etree.XMLParser(resolve_entities=True) # Noncompliant
-tree1 = etree.parse('ressources/xxe.xml', parser)
-root1 = tree1.getroot()
+filename = tempfile.mktemp() # Noncompliant
+tmp_file = open(filename, "w+")
