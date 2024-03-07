4faf706f24e3a0973f51716733f3b97d70a090e6
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2024-03-07 09:41:00 -0600

    Update S2734.py

--- src/S2734.py
+++ src/S2734.py
@@ -7,3 +7,26 @@ class MyClass(object):
 
 if not a == 2:        # Noncompliant
     b = not i < 10    # Noncompliant
+
+
+
+
+from aws_cdk import aws_ec2 as ec2
+
+instance = ec2.Instance(
+    self,
+    "my_instance",
+    instance_type=nano_t2,
+    machine_image=ec2.MachineImage.latest_amazon_linux(),
+    vpc=vpc
+)
+
+instance.connections.allow_from(
+    ec2.Peer.any_ipv4(), # Noncompliant
+    ec2.Port.tcp(22),
+    description="Allows SSH from all IPv4"
+)
+instance.connections.allow_from_any_ipv4( # Noncompliant
+    ec2.Port.tcp(3389),
+    description="Allows Terminal Server from all IPv4"
+)
